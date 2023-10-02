f99b4e54ba7ecfe91f314c3a77214c3d9434c53d
Petar Marinov <mpetar@vmware.com>
2023-10-02 08:27:48 +0300

    darwin amend comments for specific os

--- darwin/the-basics/hello.asm
+++ darwin/the-basics/hello.asm
@@ -4,11 +4,11 @@
 _main: mov X0, #1     // 1 = StdOut
         adr X1, helloworld // string to print
         mov X2, #13     // length of our string
-        mov X16, #4     // MacOS write system call
-        svc 0     // Call linux to output the string
+        mov X16, #4     // OS write system call
+        svc 0     // Call OS to output the string
 
         mov     X0, #0      // Use 0 return code
         mov     X16, #1     // Service command code 1 terminates this program
-        svc     0           // Call MacOS to terminate the program
+        svc     0           // Call OS to terminate the program
 
 helloworld:      .ascii  "Hello World!\n"
